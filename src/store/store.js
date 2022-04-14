b36742ad2a1b41bfe07a00307a1aca8da72cfb9e
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-04-14 15:12:03 +0100

    Setting up store for redux

--- src/store/store.js
+++ src/store/store.js
@@ -2,3 +2,10 @@ import { compose, createStore, appluMiddleware } from "redux";
 import logger from "redux-logger";
 
 //root-reducer
+import { rootReducer } from "./root-reducer";
+
+const middleWares = [logger];
+
+const composedEnhancers = compose(appluMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
