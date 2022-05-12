b9f89e9f41bdf9abab139da4c9e75dbaba0ac59e
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-05-12 16:19:33 +0100

    Memoization using reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryRreducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryRreducer],
+  (categorySlice) => categorySlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
