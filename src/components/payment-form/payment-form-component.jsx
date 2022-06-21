77fa95f3f776e6882d6fd5b09be843148897f5a7
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-21 16:52:53 +0100

    Completion of netlify and stripe payment

--- src/components/payment-form/payment-form-component.jsx
+++ src/components/payment-form/payment-form-component.jsx
@@ -6,13 +6,42 @@ import { PaymentFormContainer, FormContainer } from './payment-form-styles';
 const PayemntForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-  const paymentHandle = async (e) => {
+  const paymentHandler = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) return;
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        amount: 1000,
+      }),
+    }).then((res) => res.json());
+    const { client_secret } = response?.paymentIntent;
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Jenny Rosen',
+        },
+      },
+    });
+    if (paymentResult.error) {
+      alert(paymentResult.error.message);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Successful');
+      }
+    }
   };
+
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button type={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
