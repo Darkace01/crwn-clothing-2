e48f86883f184ce7093526e2586e528ed572b948
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-21 16:21:58 +0100

    payment form component

---
+++ src/components/payment-form/payment-form-component.jsx
@@ -0,0 +1,23 @@
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+
+import { PaymentFormContainer, FormContainer } from './payment-form-styles';
+
+const PayemntForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const paymentHandle = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) return;
+  };
+  return (
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <Button type={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+export default PayemntForm;
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,38 +1,40 @@
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 import {
   selectCartItems,
   selectCartTotal,
-} from "../../store/cart/cart.selector";
-import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+} from '../../store/cart/cart.selector';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PayemntForm from '../../components/payment-form/payment-form-component';
 
-import "./checkout.styles.scss";
+import './checkout.styles.scss';
 
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
   return (
-    <div className="checkout-container">
-      <div className="checkout-header">
-        <div className="header-block">
+    <div className='checkout-container'>
+      <div className='checkout-header'>
+        <div className='header-block'>
           <span>Product</span>
         </div>
-        <div className="header-block">
+        <div className='header-block'>
           <span>Description</span>
         </div>
-        <div className="header-block">
+        <div className='header-block'>
           <span>Quantity</span>
         </div>
-        <div className="header-block">
+        <div className='header-block'>
           <span>Price</span>
         </div>
-        <div className="header-block">
+        <div className='header-block'>
           <span>Remove</span>
         </div>
       </div>
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <span className="total">Total: ${cartTotal}</span>
+      <span className='total'>Total: ${cartTotal}</span>
+      <PayemntForm />
     </div>
   );
 };
